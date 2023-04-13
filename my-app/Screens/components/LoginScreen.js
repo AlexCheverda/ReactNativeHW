f770f5819cd4b39b53930af4911ec67b96d86d68
Aleksandr Cheverda <103459491+AlexCheverda@users.noreply.github.com>
2023-04-13 16:01:45 +0300

    Update LoginScreen.js
    Розмітка форми

--- my-app/Screens/components/LoginScreen.js
+++ my-app/Screens/components/LoginScreen.js
@@ -1,18 +1,81 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput, ImageBackground, KeyboardAvoidingView, Platform } from 'react-native';
+import { 
+    StyleSheet, 
+    Alert,
+    View, 
+    Text, 
+    TextInput, 
+    ImageBackground, 
+    KeyboardAvoidingView,
+    Keyboard,
+    TouchableWithoutFeedback, 
+    Platform, 
+    TouchableOpacity, 
+} from 'react-native';
+
+const initialState = {
+    login: '',
+    email: '',
+}
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const emailHandler = (text) => setEmail(text);
+    const passwordHandler = (text) => setPassword(text);
+
+    const onLogin = () => {
+        Alert.alert("Credentials", `${email} + ${password}`);
+    }
     return (
-        <View style={styles.container}>
-          <ImageBackground style={styles.image} source={require('../../assets/images/photoBG.png')}>
-            <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} keyboardVerticalOffset={-35}>
-                <View>
-                    <Text style={styles.titleText}>Login</Text>
-                    <TextInput style={styles.input}/>
-                </View>
-            </KeyboardAvoidingView>
-          </ImageBackground>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+            <ImageBackground
+               style={styles.image}
+               source={require('../../assets/images/photoBG.png')}
+            >
+                
+                <KeyboardAvoidingView
+                    behavior={Platform.OS == "ios" ? "padding" : "height"}
+                    keyboardVerticalOffset={-35}
+                >
+                    <View style={styles.form}>
+                    <Text style={styles.formTitle}>Войти</Text>
+                        <View>
+                            <TextInput
+                               value={email}
+                               onChangeText={emailHandler}
+                               style={styles.input}
+                               placeholder="Адрес электронной почты"
+                            />
+                        </View>
+                        <View>
+                            <TextInput
+                               value={password}
+                               onChangeText={passwordHandler}
+                               style={styles.input}
+                               placeholder="Пароль"
+                               secureTextEntry={true}
+                            />
+                        </View>
+                        {/* <Button title="Войти" style={styles.btn}  /> */}
+                        <TouchableOpacity
+                            activeOpacity={0.5}
+                            style={styles.btn}
+                            onPress={onLogin}
+                        >
+                            <Text style={styles.titleBtn}>Войти</Text>
+                        </TouchableOpacity> 
+                        <View>
+                            <Text>Нет аккаунта? Зарегистрироваться</Text>
+                        </View>  
+                    </View>
+                </KeyboardAvoidingView>
+            </ImageBackground>
         </View>
+        </TouchableWithoutFeedback>
+        
       );
 };
 
@@ -28,8 +91,12 @@ const styles = StyleSheet.create({
         resizeMode: "cover",
         justifyContent: "flex-end",
     },
-    titleText: {
-        fontFamily: "robotoMedium",
+    form: {
+        marginHorizontal: 16,
+        backgroundColor: "#fff",
+    },
+    formTitle: {
+        // fontFamily: "robotoMedium",
         fontSize: 30,
         lineHeight: 35.16,
         alignSelf: "center",
@@ -48,5 +115,15 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         lineHeight: 18.75,
         padding: 16,
-      },
+        marginBottom: 16,
+    },
+    btn: {
+        backgroundColor: "#FF6C00",
+        padding: 16,
+        borderRadius: 100,
+        marginTop: 43,
+    },
+    titleBtn: {
+        color: "#fff",
+    }
   });
