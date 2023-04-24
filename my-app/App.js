4ffc5ca942595233f7f31b3e403401007655d328
Aleksandr Cheverda <103459491+AlexCheverda@users.noreply.github.com>
2023-04-24 23:59:27 +0300

    upd files

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-// import { View, StyleSheet } from 'react-native';
+import {} from 'react-native';
 // import { StatusBar } from 'expo-status-bar';
-import { useRoute } from './router';
+
 import * as Font from "expo-font";
 // import { AppLoading } from 'expo';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { useRoute } from './router';
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
--- my-app/router.js
+++ my-app/router.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const MainStack = createStackNavigator();
+const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
 import LoginScreen from './Screens/components/LoginScreen';
@@ -10,38 +10,105 @@ import RegistrationScreen from './Screens/components/RegistrationScreen';
 import PostsScreen from './Screens/components/PostsScreen';
 import CreatePostsScreen from './Screens/components/CreatePostsScreen';
 import ProfileScreen from './Screens/components/ProfileScreen';
-import CommentsScreen from "./Screens/components/CommentsScreen";
-import MapScreen from './Screens/components/MapScreen';
-import Home from "./Screens/components/Home";
+// import CommentsScreen from "./Screens/components/CommentsScreen";
+// import MapScreen from './Screens/components/MapScreen';
+// import Home from "./Screens/components/Home";
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 
 export const useRoute = (isAuth) => {
     if (!isAuth) {
       return (
-        <MainStack.Navigator>
-          <MainStack.Screen
+        <AuthStack.Navigator>
+          <AuthStack.Screen
            name="Login"
            component={LoginScreen}
-           options={{ headerShown: false }}
+           options={{ headerShown: false, }}
           />
-          <MainStack.Screen
+          <AuthStack.Screen
            name="Register"
            component={RegistrationScreen}
-           options={{ headerShown: false }}
+           options={{ headerShown: false, }}
           />
-        </MainStack.Navigator>
+        </AuthStack.Navigator>
       );      
     }
     return (
       <MainTab.Navigator tabBarOptions={{ showLabel: false }}>
         <MainTab.Screen
-         options={{tabBarIcon: ({ focused, size, color }) => 
-         <MaterialCommunityIcons name="post" size={size} color={color} />,
-         }}
-         name='Posts'
-         component={PostsScreen}
+          options={{
+            tabBarIcon: ({ focused, size, color }) => (
+              <MaterialCommunityIcons
+                name="postage-stamp"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+          name='Posts'
+          component={PostsScreen}
         />
-        <MainTab.Screen options={{}} name='Create' component={CreatePostsScreen} />
-        <MainTab.Screen options={{}} name='Profile' component={ProfileScreen} />
+        <MainTab.Screen
+          options={{
+            tabBarIcon: ({ focused, size, color }) => (
+              <AntDesign
+                name="pluscircleo"
+                size={35}
+                color={color}
+              />
+            ),
+         }}
+         name='Create'
+         component={CreatePostsScreen}
+        />
+        <MainTab.Screen
+          options={{
+            tabBarIcon: ({focused, size, color}) => (
+              <MaterialCommunityIcons
+                name="face-recognition"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+          name='Profile'
+          component={ProfileScreen}
+        />
       </MainTab.Navigator> 
     );   
   }; 
+
+          {/* <MainTab.Screen
+         options={{
+           headerShown: false,
+           tabBarIcon:({focused, size, color }) => 
+          <FontAwesome
+            name="comments" size={size} color={color}
+          />
+         }}
+         name="Comments"
+         component={CommentsScreen}
+        />
+        <MainTab.Screen
+         options={{
+           headerShown: false,
+           tabBarIcon:({focused, size, color }) => 
+          <FontAwesome
+            name="map" size={size} color={color}
+          />
+         }}
+         name="Map"
+         component={MapScreen}
+        />
+        <MainTab.Screen
+         options={{
+           headerShown: false,
+           tabBarIcon:({focused, size, color }) => 
+          <FontAwesome
+            name="home" size={size} color={color}
+          />
+         }}
+         name="Home"
+         component={Home}
+        /> */}
