ee0810c82efba44793cb3d8d42d0d5495e789ebe
Aleksandr Cheverda <103459491+AlexCheverda@users.noreply.github.com>
2023-04-26 21:49:03 +0300

    add modules

--- my-app/App.js
+++ my-app/App.js
@@ -1,39 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import {} from 'react-native';
+import React from 'react';
+import 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 // import { StatusBar } from 'expo-status-bar';
 
-import * as Font from "expo-font";
+import { useFonts } from "expo-font";
 // import { AppLoading } from 'expo';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import { useRoute } from './router';
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
+  // const [isReady, setIsReady] = useState(false);
   const routing = useRoute(true);
 
-  useEffect(() => {
-    const loadApplication = async () => {
-      try {
-        await Font.loadAsync({
-          'robotoBold': require('./assets/fonts/Roboto-Bold.ttf'),
-          'robotoLight': require('./assets/fonts/Roboto-Light.ttf'),
-          'robotoRegular': require('./assets/fonts/Roboto-Regular.ttf'),
-          'robotoMedium': require('./assets/fonts/Roboto-Medium.ttf'), 
-        });
-        setIsReady(true);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    loadApplication();
-  }, []);
-  if (!isReady) {
+  const [fontsLoad] = useFonts({
+    'robotoBold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'robotoLight': require('./assets/fonts/Roboto-Light.ttf'),
+    'robotoRegular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'robotoMedium': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
+  if (!fontsLoad) {
     return null;
-  }  
+  }
+
+    
   return ( 
-    <NavigationContainer>
-      {routing}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        {routing}
+      </NavigationContainer>
+    </Provider>
+    
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     height: '100%',
+//   },
+// });
+
+// useEffect(() => {
+//   const loadApplication = async () => {
+//     try {
+//       await Font.loadAsync({
+//         'robotoBold': require('./assets/fonts/Roboto-Bold.ttf'),
+//         'robotoLight': require('./assets/fonts/Roboto-Light.ttf'),
+//         'robotoRegular': require('./assets/fonts/Roboto-Regular.ttf'),
+//         'robotoMedium': require('./assets/fonts/Roboto-Medium.ttf'), 
+//       });
+//       setIsReady(true);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   loadApplication();
+// }, []);
+// if (!isReady) {
+//   return null;
+// }
--- my-app/router.js
+++ my-app/router.js
@@ -1,115 +1,28 @@
 import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
 
-import LoginScreen from './Screens/components/LoginScreen';
-import RegistrationScreen from './Screens/components/RegistrationScreen';
-import PostsScreen from './Screens/components/PostsScreen';
-import CreatePostsScreen from './Screens/components/CreatePostsScreen';
-import ProfileScreen from './Screens/components/ProfileScreen';
-// import CommentsScreen from "./Screens/components/CommentsScreen";
-// import MapScreen from './Screens/components/MapScreen';
-// import Home from "./Screens/components/Home";
-
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { AntDesign } from "@expo/vector-icons";
+import LoginScreen from './Screens/auth/LoginScreen';
+import RegistrationScreen from './Screens/auth/RegistrationScreen';
+import Home from './Screens/main/Home';
 
 export const useRoute = (isAuth) => {
     if (!isAuth) {
       return (
         <AuthStack.Navigator>
           <AuthStack.Screen
-           name="Login"
-           component={LoginScreen}
-           options={{ headerShown: false, }}
+            options={{ headerShown: false }}
+            name="Register"
+            component={RegistrationScreen}          
           />
           <AuthStack.Screen
-           name="Register"
-           component={RegistrationScreen}
-           options={{ headerShown: false, }}
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}     
           />
         </AuthStack.Navigator>
       );      
     }
-    return (
-      <MainTab.Navigator tabBarOptions={{ showLabel: false }}>
-        <MainTab.Screen
-          options={{
-            tabBarIcon: ({ focused, size, color }) => (
-              <MaterialCommunityIcons
-                name="postage-stamp"
-                size={size}
-                color={color}
-              />
-            ),
-          }}
-          name='Posts'
-          component={PostsScreen}
-          // source={require('./assets/images/grid.png')}
-        />
-        <MainTab.Screen
-          options={{
-            tabBarIcon: ({ focused, size, color }) => (
-              <AntDesign
-                name="pluscircleo"
-                size={35}
-                color={color}
-              />
-            ),
-         }}
-         name='Create'
-         component={CreatePostsScreen}
-        />
-        <MainTab.Screen
-          options={{
-            tabBarIcon: ({focused, size, color}) => (
-              <MaterialCommunityIcons
-                name="face-recognition"
-                size={size}
-                color={color}
-              />
-            ),
-          }}
-          name='Profile'
-          component={ProfileScreen}
-        />
-      </MainTab.Navigator> 
-    );   
-  }; 
-
-          {/* <MainTab.Screen
-         options={{
-           headerShown: false,
-           tabBarIcon:({focused, size, color }) => 
-          <FontAwesome
-            name="comments" size={size} color={color}
-          />
-         }}
-         name="Comments"
-         component={CommentsScreen}
-        />
-        <MainTab.Screen
-         options={{
-           headerShown: false,
-           tabBarIcon:({focused, size, color }) => 
-          <FontAwesome
-            name="map" size={size} color={color}
-          />
-         }}
-         name="Map"
-         component={MapScreen}
-        />
-        <MainTab.Screen
-         options={{
-           headerShown: false,
-           tabBarIcon:({focused, size, color }) => 
-          <FontAwesome
-            name="home" size={size} color={color}
-          />
-         }}
-         name="Home"
-         component={Home}
-        /> */}
+  return <Home />;
+};
