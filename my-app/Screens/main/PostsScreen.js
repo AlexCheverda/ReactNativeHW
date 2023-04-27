045b85b4e444a02921a4a798cd30f2613c2807ca
Aleksandr Cheverda <103459491+AlexCheverda@users.noreply.github.com>
2023-04-27 23:31:44 +0300

    Update PostsScreen.js

--- my-app/Screens/main/PostsScreen.js
+++ my-app/Screens/main/PostsScreen.js
@@ -1,20 +1,63 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Image, Button } from "react-native";
+import { 
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    Button, 
+    FlatList
+} from "react-native";
+import { Feather, AntDesign } from '@expo/vector-icons';
 
+const PostsScreen = ({ route, navigation }) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+      if (route.params) {
+        setPosts((prevState) => [...prevState, route.params]);
+      }
+    }, [route.params]);
 
-const PostsScreen = () => {
     return (
-        <View>
-        <Text>PostsScreen</Text>
-    </View>
+      <View style={styles.container}>
+        <FlatList
+          data={posts}
+          keyExtractor={(item, indx) => indx.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.imagesContainer}>
+              <Image
+                source={{ uri: item.photo }}
+                style={{ width: 350, height: 200 }}
+              />
+              <Text
+                title="go to Comments"
+                onPress={() => navigation.navigate("CommentsScreen")}
+              >
+                <Feather name="message-circle" size={24} color="black" />
+              </Text>
+              <Text
+                title="go to map"
+                onPress={() => navigation.navigate("MapScreen")}
+              >
+                <AntDesign name="enviromento" size={24} color="black" />
+              </Text>
+            </View>
+          )}
+        />
+      </View>
     );
-};
+  };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: '#fff',
+    },
+    imagesContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 15,
     },
 });
 
